d7b985ce70195946f872aa0871808edbbf8804df
Steeve Joseph <steevejoseph@knights.ucf.edu>
2019-04-21 09:06:08 -0400

    Send intro email when user first signs up

--- middleware/user.js
+++ middleware/user.js
@@ -1,8 +1,51 @@
+/* eslint-disable no-unused-vars */
 const mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
 
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
 
 const User = require('../models/user');
+
+const sendIntroEmail = async (email) => {
+  // Sends a welcome email after user signs up
+  nodemailer.createTestAccount((createAccountError, account) => {
+    if (createAccountError) {
+      console.log(createAccountError);
+    }
+
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      secure: false,
+      service: 'gmail',
+      auth: {
+        user: process.env.SHMOOD_EMAIL_USER,
+        pass: process.env.SHMOOD_EMAIL_PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: '"Your new favorite music curator app" <shmoodapp@gmail.com>',
+      to: email,
+      subject: 'Thanks for using shmood! :)',
+      html: `<h1>Hi there!</h1>
+             <p>Thank you for trying out shmood, I hope you like it!
+                If you run into issues, have questions, or would like to contribute,
+                feel free to contact me (Steeve) at steevejoseph@knights.ucf.edu
+             </p>
+            `,
+    };
+
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.log(err);
+      }
+
+      console.log('Message send: %s', info.messageId);
+      // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    });
+  });
+};
 
 exports.addUserToDb = async (res) => {
   const { display_name, email, id } = res.body;
@@ -14,13 +57,16 @@ exports.addUserToDb = async (res) => {
     user.lastLogin = Date.now();
     await user.save();
   } else {
-    await new User({
+    const newUser = await new User({
       name: display_name,
       email,
       spotifyId: id,
       loggedIn: true,
       lastLogin: Date.now(),
-    }).save();
+    });
+
+    sendIntroEmail(newUser.email);
+    await newUser.save();
   }
 };
 
