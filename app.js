382c7385633574120a4c6cdd2a571de916f20efe
Steeve Joseph <steevejoseph@knights.ucf.edu>
2019-08-17 07:57:30 -0400

    Update limit for json transmission

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.set('view engine', 'ejs');
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: false, limit: '10mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
